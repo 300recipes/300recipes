7d81cf7329f07ae0f70a8ae0f0780638f791fbdc
Oleksandr Faryna <sasha.faryna1234@gmail.com>
2021-02-18 23:59:42 +0200

    recipe item template

--- frontend/src/main/web/src/modules/main/main.module.ts
+++ frontend/src/main/web/src/modules/main/main.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ReceiptItemComponent } from './receipt-item/receipt-item.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ReceiptItemComponent],
   exports: [
     MainPageComponent
   ],
---
+++ frontend/src/main/web/src/modules/main/receipt-item/receipt-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipt-item',
+  templateUrl: './receipt-item.component.html',
+  styleUrls: ['./receipt-item.component.css']
+})
+export class ReceiptItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
