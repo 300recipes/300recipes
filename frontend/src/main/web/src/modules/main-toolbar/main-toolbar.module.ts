efbb9b51bd317a9a0ff9eb09aa966c19a57332f8
Nikita <niverovskiinn@gmail.com>
2021-02-18 20:57:51 +0200

    sign up component

--- frontend/src/main/web/src/modules/main-toolbar/main-toolbar.module.ts
+++ frontend/src/main/web/src/modules/main-toolbar/main-toolbar.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainToolbarComponent } from './main-toolbar.component';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 
 @NgModule({
-  declarations: [MainToolbarComponent],
+  declarations: [MainToolbarComponent, SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
   ],
