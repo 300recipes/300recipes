d40c6edff5daa1765189aabf30ce13f4204cc436
Oleksandr Faryna <sasha.faryna1234@gmail.com>
2021-04-16 10:36:48 +0300

    sorting

--- frontend/src/main/web/src/modules/main/components/main-page/main-page.component.ts
+++ frontend/src/main/web/src/modules/main/components/main-page/main-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ReceiptService } from "../../../core/services/receipt.service";
-import { Observable } from "rxjs";
-import { Receipt, SearchReceipt } from "../../../core/models/receipt.model";
+import {Component, OnInit} from '@angular/core';
+import {ReceiptService} from '../../../core/services/receipt.service';
+import {Receipt, SearchReceipt} from '../../../core/models/receipt.model';
+import {DropdownItem} from '../dropdown/dropdown.component';
 
 @Component({
   selector: 'app-landing-page',
@@ -11,18 +11,59 @@ import { Receipt, SearchReceipt } from "../../../core/models/receipt.model";
 
 export class MainPageComponent implements OnInit {
   // tslint:disable-next-line:variable-name
-  //public _receiptList$: Observable<Receipt[]> = this.receiptService.getReceiptList();
+  // public _receiptList$: Observable<Receipt[]> = this.receiptService.getReceiptList();
 
   public receipts: Receipt[] = [];
 
+  public sortedReceipts: Receipt[] = [];
+
   public searchQuery = '';
 
-  constructor(private receiptService: ReceiptService) { }
+  public sortOptions: DropdownItem[] = [
+    {
+      id: 'name',
+      label: 'name',
+    },
+    {
+      id: 'author',
+      label: 'author',
+    }
+  ];
+
+  public dropdownLabel = 'Sort by: name';
+  public ascending = true;
+
+  constructor(private receiptService: ReceiptService) {
+  }
 
   ngOnInit(): void {
     this.receiptService.getReceiptList().subscribe(data => {
       this.receipts = data;
+      this.sortedReceipts = this.receipts;
+
+      this.selectSort(this.sortOptions[0]);
     });
+  }
+
+  public changeDirection() {
+    this.ascending = !this.ascending;
+    this.sortedReceipts.reverse();
+  }
+
+  public selectSort(sort: DropdownItem) {
+    if (sort.id === 'name') {
+      this.sortedReceipts = this.receipts;
+      this.sortedReceipts = this.sortedReceipts.sort(
+        (a, b) => a.title.toLocaleLowerCase().localeCompare(b.title.toLocaleLowerCase()));
+    } else if (sort.id === 'author') {
+      this.sortedReceipts = this.receipts;
+      this.sortedReceipts = this.sortedReceipts.sort(
+        (a, b) => a.author.toLocaleLowerCase().localeCompare(b.author.toLocaleLowerCase()));
+    }
+    if (!this.ascending) {
+      this.sortedReceipts.reverse();
+    }
+    this.dropdownLabel = 'Sort by: ' + sort.label;
   }
 
   public search(s: SearchReceipt): void {
